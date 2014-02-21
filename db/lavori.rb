84ee0e38d171670865d6f2acf0594c651ac9e208
Francesco Canessa <makevoid@gmail.com>
2014-02-21 13:39:40 +0100

    add photos

--- db/lavori.rb
+++ db/lavori.rb
@@ -1,1 +1,227 @@
-lavori.rb
+intro_1 = <<STR
+
+STR
+
+desc_1 = <<STR
+
+STR
+
+intro_5 = <<STR
+**Nome:** Programma di rimboschimenti per la produzione di legna da ardere ed attività divulgative
+**Ubicazione:** Baringo - Kenya
+**Anni:** 1987-1989
+**Bilancio totale approvato (US $):** 1.018.000
+**Paese finanziatore:** Australia
+**Controparte:** Ministero dell'Ambiente e delle Risorse Naturali - Dipartimento delle Foreste
+
+La deforestazione in Kenya ha assunto nel tempo dimensioni tali da accelerare il processo di degrado dell'ambiente (perdita della fertilità del terreno, inondazioni, insabbiamento, scomparsa di habitat importanti per la varietà biologica delle specie presenti) con conseguenti ripercussioni negative sulle condizioni di vita della popolazione. Tale distruzione è causata dall'incremento della popolazione e dalla povertà. Le risorse forestali (foreste naturali e rimboschimenti) sono oggetto di tagli abusivi ed anche minacciate di distruzione dagli incendi boschivi e dal pascolo intensivo.
+Il progetto aveva come obiettivo principale la promozione di una gestione sostenibile della foresta naturale nell'area di Baringo, la creazione di nuove risorse forestali mediante lo sviluppo di sistemi agro-silvo-pastorali, la piantagione di specie multi-uso, la promozione delle capacità gestionali mediante l'organizzazione di corsi di formazione per il personale locale.
+
+STR
+
+desc_5 = <<STR
+Gli interventi realizzati dal progetto sono stati:
+
+- la tutela delle foreste esistenti (dagli incendi, dalle malattie e dai tagli abusivi);
+- l'elaborazione di piani di gestione integrata a medio e a lungo termine dei boschi oggetto di tutela ed in modo da ottimizzarne le funzioni dirette (protezione del terreno, della biodiversità, fornitura di legname ed altri prodotti forestali, ecc.) ed indirette (valori estetici, culturali, ecc.) ;
+- la creazione di nuove risorse forestali mediante il rimboschimernto e la piantagione di alberi (Acacia spp. Combretum, Terminalia, Prosopis juliflora, Parkinsonia aculeata, ecc.) ed arbusti multi-uso nelle aree agricole o pastorali per la produzione di legname da opera, legna da ardere e di foraggio;
+- lo sviluppo di attività di promozione degli interventi di piantagione degli alberi forestali da parte degli agricoltori, la realizzazione di piccoli vivai forestali;
+- la formazione del personale;
+- l'avvio di ricerche sulle tecniche più appropriate di piantagione e sull'impiego di specie adatte;
+- la ricerca di fondi e di sistemi per acquisire futuri investimenti;
+- la divulgazione dei dati, dei risultati ed informazioni sul progetto.
+STR
+
+
+intro_6 = <<STR
+Nome: Programma di stabilizzazione e di fissazione delle dune
+Ubicazione: nord, est, sud-ovest del paese - Mauritania
+Anni: 1983-1989
+Bilancio totale approvato (US $) : 6.325.000
+Paese finanziatore: vedi descrizione
+Controparte: Ministero dello Sviluppo Rurale - Direzione della Protezione della Natura
+
+La Mauritania è una terra difficile: 398.000 miglia quadrate (cioè 1 milione e 31.000 Km2) di deserto battuto da venti violenti. Una popolazione di poco più di 2 milioni di abitanti per lo più arabo-berberi (mauri) ed in minor parte africani neri che vivono raccolti in piccoli centri e non più nomadi. A parte Nouakchott (la capitale) le altre città storiche come Cinguetti, Tichitt e Ouedane (protette dall’UNESCO) che un tempo erano famosi centri carovanieri, oasi ricche di palme, di acqua oggi sono state quasi abbandonate dagli abitanti stremati da carestie, siccità, isolamento.
+La copertura vegetale di circa 12 milioni di ettari negli ultimi 20 anni è quasi scomparsa; le dune prive di vegetazione si spostano con i venti e si depositano in aree abitate (villaggi, oasi), nei campi coltivati, o sulle infrastrutture creando grandi problemi alla popolazione.
+Il progetto di stabilizzazione e di fissazione delle dune è nato in questo contesto per attuare interventi urgenti nelle località più minacciate dall’insabbiamento ed avviare un programma di pianificazione e di formazione del personale in modo da rendere il Governo della Mauritania in grado da attuare da solo la lotta contro la desertificazione.
+STR
+
+desc_6 = <<STR
+Ubicazione: Azougui, Cinguetti, Guerou, Toungad (Nord,Est) Kankossa, Maghta Lajar (est)Aouinat ez Zbil, Kiffa, Mederdra, Tamchakett, Timbedra (est e sud-ovest), Aghchourguitt, Ajouer, Boutilimit (“Route de l’Espoir”).
+
+Donatori:
+UNSO (Fondo delle Nazioni Unite per il Sahel): US.$ 2.863.579
+FENU (Fondo delle Nazioni Unite per Equipaggiamento): US $ 1.398.000
+UNDP (Fondo delle Nazioni Unite per lo Sviluppo: US $ 557.000
+Algeria (Finanziamento delle borse di studio): US$ 996.000
+Governo della Mauritania : US $ 327.000
+PAM (Programma Alimentare Mondiale) US $ 183.000
+TOTALE US $ 6.325.000
+
+Il progetto ha realizzato interventi importanti e cioè:
+- protezione dall’insabbiamento di 15 aree, considerate rappresentative della principale tipologia di insabbiamento (4 oasi nel nord e centro-est del paese; 2 villaggi con aree agricole nell’area silvo-pastorale; 4 aree lungo la “Route de l’Espoir” principale asse stradale del Paese lunga 1.099 Km ed avente un ruolo vitale per 460.000 abitanti residenti e 400.000 nomadi; 5 centri abitati importanti dal punto di vista amministrativo e commerciale.
+Le tecniche adottate hanno consistito: nella stabilizzazione meccanica delle dune alte da 3 -6 metri a 30 metri) mediante la creazione di barriere artificiali (superficie trattata 1.163 ha);
+nella fissazione delle dune con il rimboschimento delle dune (superficie 1.413 ha) o la semina di specie vegetali autoctone arboree, arbustive o erbacee; protezione delle aree oggetto degli interventi.
+
+L’impatto del progetto sulla popolazione e sul territorio ha determinato:
+- il miglioramento delle condizioni di vita di 80.710 abitanti e 130 ha di habitat umani (case, pozzi, strade);
+- la protezione di 1.700 ha di aree agricole (palmeti, aree coltivate a cereali) e il miglioramento dei pascoli con importanti vantaggi economici e sociali;
+- la protezione di 10.950 m lineari di fronte delle dune in movimento lungo la “Route de l’Espoir” con risparmio di US$ 1.100.000 all’anno sul costo per le operazioni di disinsabbiamento della strada e miglioramento della circolazione stradale;
+- la produzione di legname e di foraggio con il rimboschimento di 894 ha utilizzando Prosopis ssp, Parkinsonia ssp, Leptadenia spp;
+- la formazione di 23 unità a livello universitario, di 30 tecnici e 200 operai in tecniche di stabilizzazione meccanica delle dune, piantagione, vivaismo;
+- la ricerca e la sperimentazione in tecniche di fissazione delle dune, tecniche di riproduzione delle piante utilizzate per i rimboschimenti e delle piante impiegate per la stabilizzazione meccanica delle dune;
+- la sensibilizzazione delle popolazioni locali alla lotta contro la desertificazione.
+STR
+
+
+intro_7 = <<STR
+Nome: Centro di formazione nella gestione integrata delle “mornes”
+Ubicazione: Limbé - Haiti
+Anni: 1987-1989
+Bilancio totale approvato (US $) : 1.085.000
+Paese finanziatore: Governo della Svizzera
+Controparte: Ministero dell’Agricoltura, delle Risorse Naturali e dello Sviluppo Rurale.
+
+La maggioranza della popolazione (8,3 milioni di abitanti) vive in condizioni di povertà estrema.
+L’agricoltura itinerante, l’uso intensivo delle terre marginali nei bacini imbriferi hanno causato danni ambientali talvolta irreparabili. Le foreste naturali sono state quasi totalmente distrutte ( nel 1940 coprivano il 40% del territorio ed attualmente solo l’1%).
+Le conseguenze della deforestazione sono: la perdita della fertilità del terreno, l’insabbiamento delle aree agricole, l’accelerazione del fenomeno della desertificazione, il verificarsi di inondazioni e di fenomeni erosivi diffusi.
+Il progetto ha finanziato l’assistenza tecnica al Centro di formazione di Limbè per il personale tecnico in materia di conservazione dei suoli.
+STR
+
+desc_7 = <<STR
+I risultati del progetto sono i seguenti:
+
+- assistenza tecnica al personale direttivo del Centro di Limbè per l’elaborazione di programmi di formazione in materia di conservazione del suolo, gestione dei bacini montani, tecniche di lotta all’erosione, sistemazioni idraulico-forestali, sistemi agro-forestali;
+- organizzazione di corsi di vario tipo e durata presso il Centro di Limbè per la formazione di tecnici specializzati in materia di conservazione dei suoli e con il coinvolgimento delle ONG locali ;
+- sviluppo di centri per la dimostrazione e la divulgazione di tecniche specifiche di difesa per la conservazione dei suoli e per la promozione della piantagione di alberi fra gli agricoltori;
+- sviluppo di ricerche specifiche ( selezione delle specie da piantare, tecniche di conservazione dei suoli, organizzazione e pianificazione dello sfruttamento dei terreni agricoli, sistemi agro-forestali, consolidamento di pendici franose) e elaborazione di studi (impatto delle sistemazioni idraulico-forestali sul miglioramento della fertilità dei terreni, tecniche di piantagione e colturali dei rimboschimenti, piani di gestione delle foreste esistenti).
+STR
+
+
+intro_8 = <<STR
+Nome: Assistenza tecnica alla Divisione delle Foreste
+Ubicazione: Maseru - Lesotho
+Anni: 1982-1988
+Bilancio totale approvato (US $) : 2.886.000
+Paese finanziatore: Governo della Svezia
+Controparte: Divisione delle Foreste
+
+Il progetto è nato per rafforzare la capacità istituzionale della Divisione delle Foreste formare il personale in modo da promuovere programmi di sviluppo forestale per arrestare il degrado del territorio soggetto ad erosione e per incrementare le risorse forestali.
+STR
+
+desc_8 = <<STR
+Il progetto ha ottenuto i seguenti risultati:
+
+- ha fornito assistenza tecnica alla Divisione delle Foreste, principale istituzione incaricata della gestione forestale, per quanto concerne: la legislazione forestale, la tutela e la gestione dei boschi, i rimboschimenti, le attività di conservazione del suolo e la lotta all’erosione, il miglioramento delle capacità di efficienza e gestionali, la promozione di investimenti nel settore forestale, l’avvio di attività di informazione e di divulgazione forestale con le scuole, gli agricoltori;
+- ha fornito assistenza tecnica al Lesotho Agricultural College di Maseru per la formazione del personale tecnico forestale mediante lo sviluppo di programmi di insegnamento in varie discipline, il miglioramento dei mezzi di insegnamento (materiale didattico, laboratori di ricerca, biblioteca), il perfezionamento del sistema di controllo e di valutazione dei programmi di insegnamento, l’avvio di programmi divulgativi concernenti la partecipazione delle comunità rurali allo sviluppo forestale, i sistemi e le tecniche di lotta all’erosione);
+- ha finanziato quattro borse di studio a livello universitario all’estero.
+STR
+
+
+intro_9 = <<STR
+Nome: Sviluppo forestale per la protezione del territorio e la produzione di legname
+Ubicazione: Tongyu, Jinxian, Zhangutai - Regione di Jilin - Cina
+Anni: 1989-1991
+Paese finanziatore: Governo del Belgio
+Controparte: Ministero delle Foreste
+
+Il patrimonio forestale naturale negli ultimi 40 anni è stato quasi totalmente distrutto a causa dello sviluppo eccessivo delle infrastrutture.
+Gli interventi di ricostituzione delle risorse forestali sono una componente importante della politica forestale; sono stati infatti realizzati programmi nazionali di rimboschimento su vasta scala come “The Green Great Wall “che è stato attuato su una superficie di 3.470.000 Km2, è esteso per 7.000 Km ed interessa 12 province o come “Il Great Plans” , nell’ambito del quale sono stati rimboschiti 100 milioni di ha con tecniche agro-forestali . I rimboschimenti prevedono l’impiego di specie a rapido accrescimento o multi-uso, piantagioni miste di varie specie in associazione con colture agricole con una componente di protezione del suolo e dell’ecosistema, turni brevi, utilizzazioni intensive.
+L’obiettivo principale del progetto era il rimboschimento di un’area di 5.000 ha ,nella provincia di Jilin, soggetta ad erosione eolica ed idrica per proteggere le aree coltivate e ricostituire le risorse forestali per la produzione di legname.
+STR
+
+desc_9 = <<STR
+Il progetto ha realizzato i seguenti interventi:
+
+- produzione di materiale di propagazione forestale da utilizzare per i rimboschimenti ed impianto di vivai forestali;
+- piantagione di Populus ssp. (specie indigene ed introdotte come Populus nigra var. italica, Populus euroamericana, ed altre) con tecniche di piantagione meccanizzata, di Pinus sylvestris, Robinia pseudoacacia, Salix ssp per utilizzare e migliorare il terreno, produrre legname ed incrementare la produzione agricola e foraggera;
+- formazione del personale a livello locale in tecniche di piantagione, cure colturali, gestione dei rimboschimenti, colture agroforestali, vivai forestali, ecc. ;
+- avvio di ricerche, in collaborazione con le istituzioni locali (Academy of Forestry, Institute of Forestry and Soil Science), sul miglioramento genetico e la selezione del pioppo, le tecniche di piantagione, le cure colturali dei rimboschimenti);
+- elaborazione di piani di gestione delle piantagioni;
+- avvio di attività di informazione e di divulgazione forestale.
+STR
+
+
+intro_10 = <<STR
+Nome: Ristrutturazione limonaia Villa Rondinelli e restauro del giardino
+Ubicazione: Fiesole (Firenze) - Italia
+Anni: 1992-1994
+
+Dopo aver deciso di lasciare la professione forestale in ambito internazionale ed aver scelto di vivere a Firenze nella limonaia progettata da Pietro Porcinai mi sono occupata degli interventi di recupero e restauro della proprietà (che era stata abbandonata ed in stato di degrado), del disegno di alcuni arredi e del restauro del giardino.
+STR
+
+desc_10 = <<STR
+La limonaia o “serra - studio”, situata nei terrazzamenti sopra Villa Rondinelli a San Domenico , perfettamente inserita nello stupendo paesaggio collinare di Fiesole, ha un giardino pensile al piano superiore collocato sul tetto del pianterreno. Pietro Porcinai l’aveva progettata per adibirla a sede di un centro educativo per l’architettura dei giardini e del paesaggio.
+Dopo un periodo di degrado e in stato di abbandono ho recuperato e restaurato la proprietà ed il giardino, in particolare mi sono occupata dei seguenti interventi :
+
+- ristrutturazione e restauro dei locali della limonaia ed edifici annessi;
+- disegno di alcuni mobili (armadi e librerie-divisori dei locali e tavoli) e scelta degli arredi;
+- lavori di restauro del giardino - parco, caratterizzato da vegetazione tipicamente mediterranea, consistenti in interventi di ripulitura, potature e cure colturali degli alberi, rimodellamento delle siepi, impianto di siepi, di arbusti ed erbacee perenni ornamentali.
+STR
+
+LAVORI = [
+  {
+    id: 1,
+    nome: "Lotta alla desertificazione, sviluppo forestale",
+    ubicazione: "Isole di Santiago e di Maio - Isole del Capo Verde",
+    anni: "1982-88",
+    introduzione: intro_5,
+    descrizione:  desc_5,
+  },
+  {
+    id: 5,
+    nome: "Programma di rimboschimenti per la produzione di legna da ardere ed attività divulgative",
+    ubicazione: "Baringo - Kenya",
+    anni: "1987-89",
+    introduzione: intro_5,
+    descrizione:  desc_5,
+  },
+  {
+    id: 6,
+    nome: "Programma di stabilizzazione e di fissazione delle dune",
+    ubicazione: "nord, est, sud-ovest del paese - Mauritania",
+    anni: "1983-89",
+    introduzione: intro_6,
+    descrizione:  desc_6,
+  },
+  {
+    id: 7,
+    nome: "Centro di formazione nella gestione integrata delle \“mornes\”",
+    ubicazione: "Limbé - Haiti",
+    anni: "1987-89",
+    introduzione: intro_7,
+    descrizione:  desc_7,
+  },
+  {
+    id: 8,
+    nome: "Assistenza tecnica alla Divisione delle Foreste",
+    ubicazione: "Maseru - Lesotho",
+    anni: "1982-88",
+    introduzione: intro_8,
+    descrizione:  desc_8,
+  },
+  {
+    id: 9,
+    nome: "Sviluppo forestale per la protezione del territorio e la produzione di legname",
+    ubicazione: "Tongyu, Jinxian, Zhangutai - Regione di Jilin - Cina",
+    anni: "1989-91",
+    introduzione: intro_9,
+    descrizione:  desc_9,
+  },
+  {
+    id: 10,
+    nome: "Ristrutturazione limonaia Villa Rondinelli e restauro del giardino",
+    ubicazione: "Fiesole (Firenze) - Italia",
+    anni: "1992-94",
+    introduzione: intro_10,
+    descrizione:  desc_10,
+  },
+]
+
+
+# Nome: Lotta alla desertificazione, sviluppo forestale
+# Ubicazione: Isole di Santiago e di Maio - Isole del Capo Verde
+# Anni: 1982-1988
+# Bilancio totale approvato (US $) : 7.147.000
+# Paese finanziatore: Belgio
+# Controparte: Ministero dello Sviluppo Rurale - Capo Verde
