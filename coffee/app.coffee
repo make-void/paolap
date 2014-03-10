9f66967656156648e397b8e0ca9195087c244a4a
Francesco Canessa <makevoid@gmail.com>
2014-03-10 09:53:59 +0100

    new logo and home restyling

--- coffee/app.coffee
+++ coffee/app.coffee
@@ -1,1 +1,50 @@
 console.log "hello coffee!"
+
+qs = document.querySelector
+
+window.cur_idx = 0
+
+# remember to add this css to the img element: transition: opacity 1s ease-in-out
+gal_anim = (elements) ->
+  time = 6000
+
+  setTimeout =>
+    images = elements
+    window.elements = elements
+    cur_idx = window.cur_idx
+    cond = cur_idx >= images.length-1
+    next_idx = if cond then 0 else cur_idx+1
+    elements[cur_idx].style.opacity = 0
+    elements[next_idx].style.opacity = 1
+    #console.log(elements[next_idx])
+    #console.log "hiding #{cur_idx}, showing #{next_idx}"
+    window.cur_idx = if cond then 0 else cur_idx + 1
+
+    gal_anim elements
+  , time
+
+gal_resize = ->
+
+
+  set_size = ->
+    bg = ".home .content .bg"
+    bg = document.querySelector bg
+    images = ".home .content .bg img"
+    images = document.querySelectorAll images
+    console.log "resize"
+    bg.style.height = images[0].height
+    console.log bg.style.height
+    console.log images[0].height
+
+  window.addEventListener 'resize', set_size
+
+  set_size()
+
+$ ->
+  $(".gallery").fancybox()
+
+  images = ".home .content .bg img"
+  images = document.querySelectorAll images
+  gal_anim images
+
+  gal_resize()
---
+++ public/js/app.js
@@ -0,0 +1,51 @@
+var gal_anim, gal_resize, qs;
+
+console.log("hello coffee!");
+
+qs = document.querySelector;
+
+window.cur_idx = 0;
+
+gal_anim = function(elements) {
+  var time,
+    _this = this;
+  time = 6000;
+  return setTimeout(function() {
+    var cond, cur_idx, images, next_idx;
+    images = elements;
+    window.elements = elements;
+    cur_idx = window.cur_idx;
+    cond = cur_idx >= images.length - 1;
+    next_idx = cond ? 0 : cur_idx + 1;
+    elements[cur_idx].style.opacity = 0;
+    elements[next_idx].style.opacity = 1;
+    window.cur_idx = cond ? 0 : cur_idx + 1;
+    return gal_anim(elements);
+  }, time);
+};
+
+gal_resize = function() {
+  var set_size;
+  set_size = function() {
+    var bg, images;
+    bg = ".home .content .bg";
+    bg = document.querySelector(bg);
+    images = ".home .content .bg img";
+    images = document.querySelectorAll(images);
+    console.log("resize");
+    bg.style.height = images[0].height;
+    console.log(bg.style.height);
+    return console.log(images[0].height);
+  };
+  window.addEventListener('resize', set_size);
+  return set_size();
+};
+
+$(function() {
+  var images;
+  $(".gallery").fancybox();
+  images = ".home .content .bg img";
+  images = document.querySelectorAll(images);
+  gal_anim(images);
+  return gal_resize();
+});
